738b118657b9aab4f81b3b411e35066f84b853de
mfschmidt <mikeschmidt@schmidtgracen.com>
2016-08-16 12:23:48 -0400

    Corrected database code to commit changes.

--- mfs_dupefiles.py
+++ mfs_dupefiles.py
@@ -7,6 +7,7 @@ import MySQLdb
 import hashlib
 import socket
 import os
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename")
@@ -20,7 +21,7 @@ if not os.path.isfile(args.filename):
     sys.exit(0)
 
 ## myhash
-## Returns an sha256 hash of the file passed
+## Returns an sha256 hash (represented as a 64-character string) of the file passed
 def myhash(pathname):
     bufsize = 1024*1024
     hasher = hashlib.sha256()
@@ -30,6 +31,7 @@ def myhash(pathname):
             hasher.update(buffer)
             buffer = thefile.read(bufsize)
     return hasher.hexdigest()
+    # This 256-bit hash is 64 8-bit characters
 
 
 db = MySQLdb.connect(host='tendril8.local', port=3306, user='filer', passwd="filepass", db="filedb")
@@ -39,15 +41,15 @@ db = MySQLdb.connect(host='tendril8.local', port=3306, user='filer', passwd="fil
 class Filerec:
     def __init__(self, pathname):
         if os.path.isfile(pathname):
-            self.fullpath = pathname
+            self.fullpath = os.path.realpath(pathname)
         else:
             return None
         self.path = os.path.dirname(self.fullpath)
         self.name = os.path.basename(self.fullpath)
         self.size = os.path.getsize(self.fullpath)
         self.host = socket.gethostname()
-        self.created = os.path.getctime(self.fullpath)
-        self.modified = os.path.getmtime(self.fullpath)
+        self.created = datetime.fromtimestamp(os.path.getctime(self.fullpath)).strftime('%Y-%m-%d %H:%M:%S')
+        self.modified = datetime.fromtimestamp(os.path.getmtime(self.fullpath)).strftime('%Y-%m-%d %H:%M:%S')
         self.sha256 = myhash(self.fullpath)
         self.problems = []
     
@@ -100,31 +102,41 @@ class Filerec:
     def modified_ok(self):
         return True
     def sha256_ok(self):
-        if len(self.sha256) != 256:
-            self.problems.append("File sha hash has {0} characters, but must have exactly 256.".format(len(self.sha256)))
+        if len(self.sha256) != 64:
+            self.problems.append("File {0} sha hash has {1} characters, but must have exactly 64.".format(self.name, len(self.sha256)))
+            self.problems.append("     HASH = {0}".format(self.sha256))
             return False
         return True
     def all_ok(self):
-        return name_ok() and path_ok() and size_ok() and host_ok() and created_ok() and modified_ok() and sha256_ok()
+        return self.name_ok() and self.path_ok() and self.size_ok() and self.host_ok() and self.created_ok() and self.modified_ok() and self.sha256_ok()
 
     def store(self):
-        if all_ok():
+        if self.all_ok():
             if len(self.problems) > 0:
                 print("Warnings:")
                 for s in self.problems:
                     print(" - {0}".format(s))
                 self.problems = []
             c = db.cursor()
-            return c.execute("""INSERT INTO files (name, path, size, host, created, modified, sha256) VALUES ({name},{path},{size},{host},{created},{modified},{sha256})""".format(
+            sqlInsertQuery = """INSERT INTO files ({fields}) VALUES (\"{name}\",\"{path}\",{size},\"{host}\",\"{created}\",\"{modified}\",\"{sha256}\")""".format(
+                fields = "name, path, size, host, created, modified, sha256",
                 name = self.name,
                 path = self.path,
                 size = self.size,
                 host = self.host,
                 created = self.created,
                 modified = self.modified,
-                sha256 = self.sha256,))
+                sha256 = self.sha256,)
+            print(sqlInsertQuery)
+            try:
+                c.execute(sqlInsertQuery)
+                db.commit()
+                return True
+            except:
+                print("All data passed validation, but the database server is not accepting our INSERT.")
+                return False
         else:
-            print("I am trying to add the file {0} to the database, but encountered problem(s):")
+            print("I am trying to add the file {0} to the database, but encountered problem(s):".format(self.fullpath))
             for s in self.problems:
                 print(" - {0}".format(s))
             self.problems = []
@@ -134,23 +146,33 @@ class Filerec:
     ## Retrieve a list of duplicate files from DB
     def listDupes(self):
         c = db.cursor()
-        c.execute("""SELECT * FROM files WHERE size={0} AND sha256={1}""".format(self.size, self.sha256))
-        return []
+        sqlSelectQuery = """SELECT * FROM files WHERE size={0} AND sha256=\"{1}\"""".format(self.size, self.sha256)
+        print(sqlSelectQuery)
+        c.execute(sqlSelectQuery)
+        return c.fetchall()
+        # TODO: Return a list of Filerec objects rather than mysqldb's ordered list. (Update printing out on line 173-175)
 
     ## addFile
     ## if an identical record does not already exist, add this file to the DB
     def addFile(self):
         return self.store()
 
+    # TODO: Figure out how to add a stringified function to just "print()" the filerec
 
 ## Run!
 # Store file characteristics in file record
 FR = Filerec(args.filename)
 mylist = FR.listDupes()
 if len(mylist) == 0:
-    FR.addFile()
-    print("{0} is unique and has been added to the database.".format(FR.name))
+    if FR.addFile() != False:
+        print("{0} is unique and has been added to the database.".format(FR.name))
+    else:
+        print("{0} was not added to the database.".format(FR.name))
 else:
+    print("Duplicate files exist:")
     for f in mylist:
-        print(f)
+        print("    on {host} : {fullpath}".format(
+            host=f[3],
+            fullpath=os.path.join(f[1],f[0])))
 
+db.close()
